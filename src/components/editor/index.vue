c1910d1684d12ebdce87645de05943102ca4e2f1
yeqiuna <yeqiuna@juzix.io>
2017-10-30 18:33:44 +0800

    2017年10月30日18:33:42

--- src/components/editor/index.vue
+++ src/components/editor/index.vue
@@ -1,21 +1,63 @@
 <template>
     <div class="">
-        <div class="tabs">
-            <div class="files">
-                <ul>
-                    <li>文件一</li>
-                    <li>文件一</li>
-                    <li>文件一</li>
-                    <li>文件一</li>
+        <div class="file-tab">
+            <div class="tabs" ref='tabs'>
+                <div class='scroll-bar left-bar' ref='leftbar' @click='scrollLeft' >
+                    <i>&lt;</i>
+                </div>
+                <ul class='files' ref='files' @click='setTab'>
+                    <li class='file'>
+                        <span>文件1</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件2</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件3</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件4</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件5</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件6</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件7</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件8</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件9</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件10</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件11</span>
+                        <span class="remove">x</span>
+                    </li>
+                    <li class='file'>
+                        <span>文件12</span>
+                        <span class="remove">x</span>
+                    </li>
                 </ul>
-            </div>
-            <div class="tool">
-                <span class='save' @click='save'>保存当前文件</span>
-                <span class="search" @click='search'>搜索</span>
-                <span class='format' @click='format' >格式化</span>
-                <span class='increase' @click='increase'>字体放大</span>
-                <span class='decrease' @click='decrease'>字体缩小</span>
-                <span class='close' @click='close'>关闭所有窗口</span>
+                <div class='scroll-bar right-bar' @click='scrollRight' ref='rightbar'>
+                    <i>&gt;</i>
+                </div>
             </div>
             <div class="search-model" v-if='searchVisible'>
                 <div class='search-content'>
@@ -32,6 +74,15 @@
                     </el-form>
                 </div>
             </div>
+
+        </div>
+        <div class="tool">
+            <span class='save' @click='save'>保存</span>
+            <span class="search" @click='search'>搜索</span>
+            <span class='format' @click='format' >格式化</span>
+            <span class='increase' @click='increase'>放大</span>
+            <span class='decrease' @click='decrease'>缩小</span>
+            <span class='close' @click='close'>关所</span>
         </div>
         <div id="javascript-editor" class='editor'></div>
     </div>
@@ -57,6 +108,7 @@
                    search:"",
                 },
                 searchVisible:false,
+                vistual:200,
 
             }
         },
@@ -114,6 +166,40 @@
             close:function(){
                 console.log('关闭所有窗口')
             },
+            //向右滑动
+            scrollRight:function(e){
+                var rightArrow = this.$refs.rightbar;
+                var leftArrow = this.$refs.leftbar;
+                var hiddenLength = this.$refs.files.offsetWidth - this.$refs.tabs.offsetWidth;
+                var currentLeft = this.$refs.files.offsetLeft || 20;
+                var hiddenRight = hiddenLength + currentLeft;
+                console.log(hiddenLength,currentLeft,hiddenRight);
+                if(hiddenRight > 0){
+                    if(hiddenRight > this.vistual){
+                       this.$refs.files.style.left = `${currentLeft - this.vistual}px`
+                    }else{
+                        this.$refs.files.style.left = `${currentLeft - hiddenRight - 50}px`
+                    }
+                }
+            },
+            //向左滑动
+            scrollLeft:function(){
+                var leftArrow = this.$refs.leftbar;
+                var rightArrow = this.$refs.rightbar;
+                var currentLeft = this.$refs.files.offsetLeft || 20;
+                console.log(currentLeft)
+                if(currentLeft < 0){
+                    if(currentLeft < -this.vistual){
+                       this.$refs.files.style.left = `${currentLeft + this.vistual}px`
+                    }else{
+                        this.$refs.files.style.left = `${currentLeft - currentLeft + 20}px`
+                    }
+                }
+            },
+            //files的tab切换
+            setTab:function(){
+
+            }
         },
         //生命周期函数
         created() {
@@ -165,22 +251,54 @@
     描述：统一使用less,局部样式
 -->
 <style lang="less" scoped>
-.tabs{
+.file-tab{
     height:30px;
     // width:100%;
     background-color:gray;
     position: relative;
-    .tool{
-        float:right;
-        padding:0 10px;
-        span{
-            display: inline-block;
-            line-height: 25px;
-            padding:0 5px;
-            border:1px solid #000;
+    overflow:hidden;
+    .tabs{
+        width:100%;
+        position: relative;
+        .scroll-bar{
+            position: absolute;
+            width:20px;
+            height:30px;
+            background:red;
+            color:#fff;
+            top:0;
+            z-index:999;
+            // height:100%;
+            font-size:16px;
             cursor: pointer;
         }
+        .left-bar{
+            left:0;
+        }
+        .right-bar{
+            right:0;
+        }
+        .files{
+            display: flex;
+            flex-wrap:nowrap;
+            flex-direction:row;
+            justify-content:flex-start;
+            height:30px;
+            line-height: 30px;
+            position: absolute;
+            left:20px;
+            overflow:hidden;
+            li{
+                width:100px;
+                border-right:1px solid #fff;
+                background-color:hsla(229, 100%, 97%, 1);
+                color:#000;
+                text-align: center;
+            }
+        }
+
     }
+
     .search-model{
         width: 460px;
         height: 40px;
@@ -193,6 +311,21 @@
         margin-left: -;
         z-index: 1000;
         margin-left: -230px;
+    }
+
+}
+.tool{
+
+    height:30px;
+    color:#fff;
+    background-color:#000;
+    span{
+        float:right;
+        display: inline-block;
+        line-height: 25px;
+        padding:0 5px;
+        border:1px solid #000;
+        cursor: pointer;
     }
 }
 .editor{
