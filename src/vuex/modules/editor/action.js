b74987e25137e3128ae210682ad061e80709218c
zhoujiawei <zhoujiawei@juzix.io>
2017-11-08 11:05:51 +0800

    2017年11月8日11:05:49

--- src/vuex/modules/editor/action.js
+++ src/vuex/modules/editor/action.js
@@ -9,6 +9,7 @@ export const editorAction = {
 		commit('UPDATE_SAVE_CODE',code)
 	},
 	updateData({commit,state},data){
+		console.log(data);
 		localStorage.setItem('editFileData',JSON.stringify(data))
 		commit('UPLOAD_EDIT_FILEDATA',data)
 	},
@@ -18,14 +19,26 @@ export const editorAction = {
 	updateActiveEditor({commit,state},data){
 		commit('UPDATE_ACTION_EDITOR',data)
 	},
-	saveEditorFile({dispatch,commit,state},cb){
+	saveEditorFile({dispatch,commit,state,rootState},cb){
 		file.saveFile(state.activeEditor.value,state.activeEditor.name,state.activeEditor.source,(err,filepath)=>{
 			if(err){
 
 			}else{
 				if(filepath){
-					console.log(state.fileData);
 					const keyId = file.keyIdFn(filepath);
+					// 更新未保存vuex的状态
+					let edit = [];
+					state.editData.forEach((item,index)=>{
+						if(item.keyId !==  state.activeEditor.keyId){
+							alert(11111);
+							edit.push(item);
+						}
+					})
+					edit = JSON.stringify(edit)
+					console.log(1111111);
+					console.log(edit);
+					dispatch('updateData',JSON.parse(edit),{ root: true });
+					
 					dispatch('updateFileData',{param:{keyId:state.activeEditor.keyId,value:filepath,name:file.basename(filepath)},id:keyId},{ root: true });
 					
 					dispatch('updateTreeData',{keyId:state.activeEditor.keyId,save:true,value:filepath,name:file.basename(filepath)},{ root: true });
@@ -42,22 +55,7 @@ export const editorAction = {
 					
 					dispatch('updateUrl',url,{ root: true });
 
-					// 更新未保存vuex的状态
-					let editData = state.editData;
-					state.editData.forEach((item,index)=>{
-						console.log(item.keyId ==  state.activeEditor.keyId);
-						if(item.keyId ==  state.activeEditor.keyId){
-							editData.splice(index,1);
-							return;
-						}
-					});
 					
-					
-					console.log(editData);
-					
-					dispatch('updateData',editData,{ root: true });
-
-
 					commit('UPDATE_ACTION_EDITOR',{  // 更新当前编辑的状态
 						value: filepath,
 						name: file.basename(filepath),
