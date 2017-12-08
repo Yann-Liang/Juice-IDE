1f0c7ebfc1056b1686c9045772528c209d215b27
liangyanxiang <liangyanxiang@juzix.io>
2017-12-08 15:28:26 +0800

    deve

--- src/services/contract-servies.js
+++ src/services/contract-servies.js
@@ -2,7 +2,7 @@
  * @Author: liangyanxiang
  * @Date: 2017-10-25 17:34:42
  * @Last Modified by: liangyanxiang
- * @Last Modified time: 2017-12-05 18:36:29
+ * @Last Modified time: 2017-12-08 10:31:39
  */
 //引入web3
 let Web3 = require('web3'),
